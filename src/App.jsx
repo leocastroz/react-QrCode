462445e989e5e0785ae27803433181418be49467
leoHcodar <suporte@horadecodar.com.br>
2023-08-04 11:48:52 -0300

    docs: Installing html5-qrcode library

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,68 @@
-// import { useState } from 'react'
-
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { Html5QrcodeScanner } from 'html5-qrcode';
+import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [scanResult, setScanResult] = useState(null);
+  const [manualSerialNumber, setManualSerialNumber] = useState('');
+
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
+
+    let isScanning = true;
+
+    scanner.render(success, error);
+
+    function success(result) {
+      if (isScanning) {
+        scanner.clear();
+        setScanResult(result);
+        isScanning = false; // Set isScanning to false to stop further scanning
+      }
+    }
+
+    function error(err) {
+      console.warn(err);
+    }
+  }, []);
+
+  function handleManualSerialNumberChange(event) {
+    setManualSerialNumber(event.target.value);
+  }
 
   return (
-    <>
-      <div>
-        <h1 className='bg-blue-200'>Reset React-app</h1>
-      </div>
-    </>
-  )
+    <div className="App">
+      <h1>QR Scanning Code</h1>
+      {scanResult ? (
+        <div>
+          <p>Success: <a href={scanResult}>{scanResult}</a></p>
+          <p>Serial Number: {scanResult.slice(-16)}</p>
+        </div>
+      ) : (
+        <div>
+          <div id="reader"></div>
+          <p className="center-text">Or enter the serial number manually:</p>
+          <div className="center-input">
+            <input
+              type="text"
+              value={manualSerialNumber}
+              onChange={handleManualSerialNumberChange}
+            />
+            {manualSerialNumber && (
+              <p>Serial Number: {manualSerialNumber.slice(-16)}</p>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
